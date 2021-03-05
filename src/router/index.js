5bdddbdc73b6867454ad85a471bec1ed5ecefee1
konnnnb <chorusaku.t@gmail.com>
2021-03-05 20:25:15 +0900

    ルーターの追加

---
+++ src/router/index.js
@@ -0,0 +1,34 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Post from '../views/Post.vue'
+import ShowPost from '../views/ShowPost.vue'
+import ShowPosts from '../views/ShowPosts.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'ShowPosts',
+    component: ShowPosts
+  },
+  {
+    path: '/showpost',
+    name: 'ShowPost',
+    component: ShowPost
+  },
+  {
+    path: '/post',
+    name: 'Post',
+    component: Post
+  },
+  
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
