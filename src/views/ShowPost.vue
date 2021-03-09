cca750a7d9b366f5b7a3c31ad78e852dc8944752
kaine-42 <kaine04021999@gmail.com>
2021-03-09 22:14:02 +0900

    マップ追加。かいね

--- src/views/ShowPost.vue
+++ src/views/ShowPost.vue
@@ -1,40 +1,45 @@
 <template>
   <div class="showPost">
     <h1>This is an about page</h1>
-    <div v-for="(post, index) in posts" v-bind:key="index">{{ post.mainTitle }}</div>
+    <div v-for="(post, index) in posts" v-bind:key="index">{{ index+1 }}: {{ post.mainTitle }}, {{ post.category }}, {{ post.mainDescription }} <img v-bind:src="post.mainImage">
+     <GmapMap
+        :center="post.startPosition.position"
+        :zoom="18"
+        style="width: 500px; height: 500px;">
+        <GmapMarker
+          :position="post.startPosition.position"
+        ></GmapMarker>
+      </GmapMap>
+      
+    </div>
+    
   </div>
 </template>
 
 <script>
 export default {
   name: 'ShowPost',
-  created: function() {
-    this.posts=[
-      {
+  data() {
+    return {
+      posts: [
+         {
         mainTitle: ("東京都府中市"),
-        // category: (category["地元"]),
+        category: ("地元"),
         mainDescription: ("地元にある大國魂神社です。"),
-        mainImage: ("../assets/sample1.jpg"),
-        // startPosition: {longitude:(int),latitude:(int),image:(string),title:(string),text:(string)},
-        // endPosition: {longitude:(int),latitude:(int),image:(string),title:(string),text:(string) },
-        // relayPosition: [{longitude:(int),latitude:(int),image:(string),title:(string),text:(string)},{longitude:(int),latitude:(int),image:(string),title:(string),text:(string)},]
+        mainImage: require("../assets/sample1.jpg"),
+        startPosition: {position:{lat:(35.66747607542623),lng:(139.47899109739402)},}//image: require(string),title:(string),text:(string)},
+        // endPosition: {position:{lat:(int),lng:(int)},image:(string),title:(string),text:(string) },
+        // relayPosition: [{position:{lat:(int),lng:(int)},image:(string),title:(string),text:(string)},{position:{lat:(int),lng:(int)},image:(string),title:(string),text:(string)},]
         },
         {
           mainTitle: ("早稲田大学"),
-          // category: (category["大学"]),
+          category: ("大学"),
           mainDescription: ("とても大きな大学です。"),
-          mainImage: ("../assets/sample2.jpg"),
+          mainImage: require("../assets/sample2.jpg"),
+          startPosition: {position:{lat:(35.709251635804335),lng:(139.72077192659762)},}//image: require(string),title:(string),text:(string)},
+          // endPosition: {position:{lat:(int),lng:(int)},image:(string),title:(string),text:(string) },
+          // relayPosition: [{position:{lat:(int),lng:(int)},image:(string),title:(string),text:(string)},{position:{lat:(int),lng:(int)},image:(string),title:(string),text:(string)},]
           },
-          ]
-  },
-  data: function() {
-    return {
-      posts: [
-        {
-          mainTitle: "",
-          mainDescription: "",
-          mainImage: "",
-        }
       ],
     }
   },
