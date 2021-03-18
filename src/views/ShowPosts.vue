93ea5c7e5dbab281118c2d1dae73bca8212c8e4c
kaine-42 <kaine04021999@gmail.com>
2021-03-19 02:28:04 +0900

    頑張ったよ。かいね

--- src/views/ShowPosts.vue
+++ src/views/ShowPosts.vue
@@ -1,38 +1,42 @@
 <template>
-  <div class="showPost">
-    <h1>This is an about page</h1>
-    <div v-for="(post, index) in posts" v-bind:key="index">
-      <h3>{{ post.mainTitle }}</h3>
-      {{ post.category }}, {{ post.mainDescription }}
-      <img v-bind:src="post.mainImage">
-     <GmapMap
-        :center="post.startPosition.position"
-        :zoom="18"
-        style="width: 500px; height: 500px;">
-        <GmapMarker
-          :position="post.startPosition.position"
-        ></GmapMarker>
-      </GmapMap>
-     <GmapMap
-        :center="post.endPosition.position"
-        :zoom="18"
-        style="width: 500px; height: 500px;">
-        <GmapMarker
-          :position="post.endPosition.position"
-        ></GmapMarker>
-      </GmapMap>
-      
-    
-      
+  <div class="showPosts">
+      <div class="showPost" v-for="(post, index) in posts" v-bind:key="index">
+        <h3>{{ post.mainTitle }}</h3>
+        <div v-for="(value, index) in category" v-bind:key="index">
+          <h4>カテゴリー：{{ value.category }}</h4>
+        </div>
+        <h5>{{ post.mainDescription }}</h5>
+        <img v-bind:src="post.mainImage">
+        <div id="hazimari">
+          <h5>始点：{{ post.startPosition.title }}</h5>
+          <GmapMap
+            :center="post.startPosition.position"
+            :zoom="18"
+            style="width: 500px; height: 500px;">
+            <GmapMarker
+              :position="post.startPosition.position"
+            ></GmapMarker>
+          </GmapMap>
+        </div>
+        <div id="owari">
+          <h5>終点：{{ post.endPosition.title }}</h5>
+          <GmapMap
+            :center="post.endPosition.position"
+            :zoom="18"
+            style="width: 500px; height: 500px;">
+            <GmapMarker
+              :position="post.endPosition.position"
+            ></GmapMarker>
+          </GmapMap>
+        </div>  
     </div>
-    
   </div>
 </template>
 
 <script>
 import firebase from "firebase"
 export default {
-  name: 'ShowPost',
+  name: 'ShowPosts',
   data() {
     return {
       posts: [
@@ -76,4 +80,15 @@ export default {
 </script>
 
 <style scoped>
+.showPost {
+  background-color: #fcf9f9;
+  box-shadow: 2px 2px 12px #e2e1e1;
+  margin-left: 250px;
+  margin-right: 250px;
+}
+
+img {
+  width: 100%;
+}
+
 </style>
