383d57a7efa191fd31cd730d2a88b62864fad384
kaine-42 <kaine04021999@gmail.com>
2021-03-19 18:52:38 +0900

    cssを調整しました

--- src/views/ShowPosts.vue
+++ src/views/ShowPosts.vue
@@ -1,34 +1,13 @@
 <template>
+
   <div class="showPosts">
       <div class="showPost" v-for="(post, index) in posts" v-bind:key="index">
-        <h3>{{ post.mainTitle }}</h3>
+        <h2>{{ post.mainTitle }}</h2>
         <div v-for="(value, index) in post.category" v-bind:key="index">
-          <h4>カテゴリー：{{ value }}</h4>
+          <h4>#{{ value }} </h4>
         </div>
         <h5>{{ post.mainDescription }}</h5>
         <img v-bind:src="post.mainImage">
-        <div id="hazimari">
-          <h5>始点：{{ post.startPosition.title }}</h5>
-          <GmapMap
-            :center="post.startPosition.position"
-            :zoom="18"
-            style="width: 500px; height: 500px;">
-            <GmapMarker
-              :position="post.startPosition.position"
-            ></GmapMarker>
-          </GmapMap>
-        </div>
-        <div id="owari">
-          <h5>終点：{{ post.endPosition.title }}</h5>
-          <GmapMap
-            :center="post.endPosition.position"
-            :zoom="18"
-            style="width: 500px; height: 500px;">
-            <GmapMarker
-              :position="post.endPosition.position"
-            ></GmapMarker>
-          </GmapMap>
-        </div>  
     </div>
   </div>
 </template>
@@ -39,26 +18,7 @@ export default {
   name: 'ShowPosts',
   data() {
     return {
-      posts: [
-        //  {
-        // mainTitle: ("東京都府中市"),
-        // category: ("地元"),
-        // mainDescription: ("地元にある大國魂神社です。"),
-        // mainImage: require("../assets/sample1.jpg"),
-        // startPosition: {position:{lat:(35.66747607542623),lng:(139.47899109739402)},image: require("../assets/sample1.jpg"),title:("a"),text:("aaa")},
-        // endPosition: {position:{lat:(35.66747607542623),lng:(139.47899109739402)},image: require("../assets/sample1.jpg"),title:("b"),text:("bbb") },
-        // relayPosition: [{position:{lat:(35.66747607542623),lng:(139.47899109739402)},image: require("../assets/sample1.jpg"),title:("c"),text:("ccc")},{position:{lat:(35.66747607542623),lng:(139.47899109739402)},image: require("../assets/sample1.jpg"),title:("d"),text:("ddd")},]
-        // },
-        // {
-        //   mainTitle: ("早稲田大学"),
-        //   category: ("大学"),
-        //   mainDescription: ("とても大きな大学です。"),
-        //   mainImage: require("../assets/sample2.jpg"),
-        //   startPosition: {position:{lat:(35.709251635804335),lng:(139.72077192659762)},image: require("../assets/sample2.jpg"),title:("1"),text:("123")},
-        //   endPosition: {position:{lat:(35.709251635804335),lng:(139.72077192659762)},image: require("../assets/sample2.jpg"),title:("2"),text:("234")},
-        //   relayPosition: [{position:{lat:(35.709251635804335),lng:(139.72077192659762)},image: require("../assets/sample2.jpg"),title:("2"),text:("234")},]
-        //   },
-      ],
+      posts: [],
     }
   },
   
@@ -80,15 +40,26 @@ export default {
 </script>
 
 <style scoped>
+.showPosts {
+  display: grid;
+  grid-template-columns: 50% 50%;
+}
+
 .showPost {
   background-color: #fcf9f9;
   box-shadow: 2px 2px 12px #e2e1e1;
-  margin-left: 250px;
-  margin-right: 250px;
+  padding: 20px;
+  margin: 10px;
 }
 
-img {
-  width: 100%;
+h2 {
+  text-align: center;
+}
+
+.showPost img {
+  width: 90%;
+  display: block;
+  margin: auto;
 }
 
 </style>
